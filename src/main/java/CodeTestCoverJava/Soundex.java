4f676fb2b957ecb3ff4dbc3bf3dc536b104e6e85
Manikandan9559 <manibcse@gmail.com>
2024-06-27 14:38:33 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -3,46 +3,52 @@ package CodeTestCoverJava;
 public class Soundex {
 
     public static String generateSoundex(String name) {
-        if (name == null || name.isEmpty()) {
+        if (StringUtils.isEmpty(name)) {
             return "";
         }
-
-        StringBuilder soundex = new StringBuilder();
+        StringBuilder soundex = buildSoundex(name);
+        while (soundex.length() < 4) {
+            soundex.append('0');
+        }
+        return soundex.toString();
+    }
+	
+	private static StringBuilder buildSoundex(String name) {
+		StringBuilder soundex = new StringBuilder();
         soundex.append(Character.toUpperCase(name.charAt(0)));
-        char prevCode = getSoundexCode(name.charAt(0));
-
+        char prevCode = getSoundexCodeNew(name.charAt(0));
         for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
-            char code = getSoundexCode(name.charAt(i));
+            char code = getSoundexCodeNew(name.charAt(i));
             if (code != '0' && code != prevCode) {
                 soundex.append(code);
                 prevCode = code;
             }
         }
+        return soundex;
+	}
 
-        while (soundex.length() < 4) {
-            soundex.append('0');
-        }
-
-        return soundex.toString();
+    private static char getSoundexCodeNew(char c) {
+    	Map<Character, Character> characterMap = buildSoundexMap();
+    	if(characterMap.containsKey(Character.toUpperCase(c))) {
+    		return characterMap.get(Character.toUpperCase(c));
+    	}
+    	return '0';
     }
-
-    private static char getSoundexCode(char c) {
-        c = Character.toUpperCase(c);
-        switch (c) {
-            case 'B': case 'F': case 'P': case 'V':
-                return '1';
-            case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z':
-                return '2';
-            case 'D': case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M': case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
-        }
+    
+    private static Map<Character, Character> buildSoundexMap() {
+    	Map<Character, Character> characterMap = new HashMap<>();
+    	characterMap.putAll(populateSoundexMap(Arrays.asList('B', 'F', 'P', 'V'), '1'));
+    	characterMap.putAll(populateSoundexMap(Arrays.asList('C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z'), '2'));
+    	characterMap.putAll(populateSoundexMap(Arrays.asList('D', 'T'), '3'));
+    	characterMap.put('L', '4');
+    	characterMap.putAll(populateSoundexMap(Arrays.asList('M', 'N'), '5'));
+    	characterMap.put('R', '6');
+    	return characterMap;
+    }
+    
+    private static Map<Character, Character> populateSoundexMap(List<Character> nameCharList, char code) {
+    	Map<Character, Character> characterMap = new HashMap<>();
+    	nameCharList.stream().forEach(nameChar -> characterMap.put(nameChar, code));
+    	return characterMap;
     }
 }
